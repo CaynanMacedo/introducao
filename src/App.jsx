81388d6402fd8e2b047909c53d9c2438c4a9b5f7
CaynanMacedo <caynanmacedo2@gmail.com>
2026-09-02 17:11:24 -0300

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 
 function App() {
+  
+  let nome = "caynan"
+  let sobrenome = "Macedo"
+  let nome_completo = nome +" "+ sobrenome
+  let clicks = 0
 
-  return (
+  function aumentarClicks(){
+    clicks += 1 
+  }
+
+  
+  return ( 
+  
     <div>
-      <h1>Hello World</h1>
-      <p>Estou aprendendo React XD</p>
+      <h1>Hello {2+2} you</h1>
+      <p>Estou aprend{console.log("foda-se")}endo React XD</p>
+      <p>Meu nome é {nome} meu sobrenome é {sobrenome} meu nome completo é {nome_completo} </p>
+
+      <hr/>
+
+      <p>Voce clickou {clicks} vezes</p>
+      <button onClick={aumentarClicks} >Clique em Mim</button>
+    
     </div>
   )
 }
